54d2c92bce0834d3722b934ffffe0c0e652f6c82
Dzerbun Igor <dzerbuni@mail.ru>
2023-07-30 23:53:38 +0300

    task 5, 6

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,6 +1,22 @@
 from utils import arrs
 
 
+def test_get(array_fixture):
+    assert arrs.get(array_fixture, 1, "test") == 2
+    assert arrs.get(array_fixture, -1, "test") == "test"
+
+
+def test_slice(array_fixture):
+    assert arrs.my_slice(array_fixture, 1, 3) == [2, 3]
+    assert arrs.my_slice(array_fixture, 1) == [2, 3, 4, 5]
+    assert arrs.my_slice(array_fixture, -2) == [4, 5]
+    assert arrs.my_slice(array_fixture, -8, 3) == [1, 2, 3]
+
+
+def test_slice_empty_array(empty_array_fixture):
+    assert arrs.my_slice(empty_array_fixture, 1) == []from utils import arrs
+
+
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], -1, "test") == "test"
